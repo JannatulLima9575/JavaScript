6a5567893d6949e8f8a1bf23dbe47c5bafd96b03
Jannatul <limajannat1675@gmail.com>
2025-02-12 20:10:50 +0600

    added user login project

---
+++ basic-project/user_login.js
@@ -0,0 +1,8 @@
+let username = "Lima";
+let password = "Code123";
+
+if (username === "Lima" && password === "Code123") {
+    console.log("Login Successful! ✅");
+} else {
+    console.log("Invalid username or password! ❌");
+}
