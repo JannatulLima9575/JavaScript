804a33032ccdfd9b1f457a0cd9816084844c3f39
Jannatul <limajannat1675@gmail.com>
2025-02-12 20:03:56 +0600

    added check temperature project

---
+++ basic-project/Temperature.js
@@ -0,0 +1,11 @@
+console.log("Temperature Checker (Hot, Warm, Cold)");
+let temperature = 30;
+
+if (temperature > 35) {
+    console.log("It's too hot! 🥵");
+} else if (temperature >= 20 && temperature <= 35) {
+    console.log("The weather is warm. 😊");
+} else {
+    console.log("It's cold! 🥶");
+}
+
