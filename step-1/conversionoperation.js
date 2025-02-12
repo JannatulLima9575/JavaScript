f268960d23f7e3285d529f128c11ffbee27e6bf8
Jannatul <limajannat1675@gmail.com>
2025-02-12 15:59:01 +0600

    added conversion

---
+++ step-1/conversionoperation.js
@@ -0,0 +1,42 @@
+let score = 50
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let position = "Third"
+console.log(typeof position);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let pass = "1234abd"
+console.log(pass);
+let upPass = Number(pass)
+console.log(typeof upPass);
+console.log(upPass);
+
+/* note */
+// "50" - 33 
+// "123abd" - NaN but type will be number 
+// true - 1 and false will be - 0
+
+
+// #about Boolean True/False 
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLogged = 0
+
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged);
+
+// true - 1 and false will be - 0
+// " " - false
+// "Lima" - true
+
+let isString = 50
+let conString = String(isString)
+console.log(conString);
+console.log(typeof conString);
--- step-1/datatypes.js
+++ step-1/datatypes.js
@@ -1,3 +1,17 @@
 let name = "Jannatul Lima"
 let age = 22
 let isLoggedIn = false
+let state;
+
+// number - ২ to power 53
+// bigint 
+// string - " "
+// Boolean - true/false 
+// null - standalone value 
+// undefined 
+// Symbol - unique
+
+// Object
+
+console.log(typeof null);
+console.log(typeof undefined);
