e654b399c37f43eaf1248e2008d758d16e46a103
Jannatul <limajannat1675@gmail.com>
2025-02-17 02:06:56 +0600

    added string Method

---
+++ conceptual-2/string.js
@@ -0,0 +1,12 @@
+// # About string 
+let firstName = "Jannatul";
+let lastName = "Lima";
+let fullName = firstName + lastName;
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+
+console.log(firstName + "", lastName);
+console.log(firstName.length);
+
+
