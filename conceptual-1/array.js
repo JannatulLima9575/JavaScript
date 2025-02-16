986841ef8bf377792c51d1072b23884520c9366d
Jannatul <limajannat1675@gmail.com>
2025-02-17 01:01:29 +0600

    added array.js file

--- conceptual-1/array.js
+++ conceptual-1/array.js
@@ -8,3 +8,21 @@ console.log(typeof thisTrio);
 console.log(thisTrio.length);
 console.log(thisTrio[2]);
 
+// # About Push, Pop, Shift $ Unshift in Array 
+let fruits = ["Apple", "Banana"];
+// work in last 
+fruits.push("Orange");
+fruits.pop();
+
+// work in first 
+fruits.unshift("litchi");
+fruits.shift();
+console.log(fruits);
+
+
+// # about slice and splice in Array 
+let marks = [20, 35, 76, 24, 67, 45, 67, 99, 40];
+let newMarks = marks.slice(2, 8);
+let newMark = marks.splice(2, 8);
+console.log(newMarks);
+console.log(newMark);
