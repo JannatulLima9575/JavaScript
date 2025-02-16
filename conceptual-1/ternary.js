f916244f96c5ca4c461bb7d7e3967b4f6aa2ceab
Jannatul <limajannat1675@gmail.com>
2025-02-17 01:17:28 +0600

    added ternary.js file

---
+++ conceptual-1/ternary.js
@@ -0,0 +1,18 @@
+// # About ternary
+let age = 20;
+let message = age >= 18 ? "You are an adult." : "You are a minor.";
+console.log(message);
+
+let num = 0;
+let result = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
+console.log(result);
+
+// practice 
+1 > 2 ? console.log(true) : console.log(false)
+
+// # practice 
+let name = "lima";
+
+name == "lima" ? console.log("yes found") : console.log("Not found");
+
+
