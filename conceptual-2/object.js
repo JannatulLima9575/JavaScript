ff38b3ca32d74e42a01727bcc572c8769c71b080
Jannatul <limajannat1675@gmail.com>
2025-02-17 14:20:57 +0600

    added object file

---
+++ conceptual-2/object.js
@@ -0,0 +1,22 @@
+const hena = {
+    fullName : "Mst Hena Khatun",
+    age : 20,
+    relationshipStatus : false,
+    friends : ['Rohim', 'Korim', 'Joba'],
+    fatherName : "Abul",
+    motherName : "Asma Khatun"
+}
+console.log(hena);
+
+const name = hena.fullName;
+console.log(name);
+
+console.log(hena.friends[1]);
+
+const key = Object.keys(hena); // for print keys
+// const key = Object.keys(hena).length;
+console.log(key);
+
+const value = Object.values(hena); // print for values
+// const value = Object.values(hena).length;
+console.log(value);
